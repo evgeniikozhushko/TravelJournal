4a68e3cbc74e3cded61eaf49e66b869fa73cf18f
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-08-07 15:05:50 -0600

    Aug 7th commit

--- src/data.jsx
+++ src/data.jsx
@@ -4,26 +4,26 @@ export default [
         location: "Japan",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "12 Jan, 2020",
-        endDate: "24 Jan, 2021",
+        endDate: "14 Jan, 2020",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        imageUrl: "../images/Mount_Fuji.jpg"
     },
     {
         title: "Mount Elbrus",
         location: "Russia",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "1 Aug, 2001",
-        endDate: "24 Aug, 2001",
-        description: "Mount Elbrus is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        googleMapsUrl: "https://goo.gl/maps/z3Rmdyk2PhsnfsaA6",
+        startDate: "1 Aug, 2002",
+        endDate: "24 Aug, 2002",
+        description: "Mount Elbrus is the highest and most prominent peak in Russia and Europe. It is situated in the western part of the Caucasus and is the highest peak of the Caucasus Mountains.",
+        imageUrl: "../images/Mount_Elbrus.jpg"
     },
     {
         title: "Mount Everest",
         location: "Nepal",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Everest is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        googleMapsUrl: "https://goo.gl/maps/EkUm6CwHXwzXZfyr9",
+        startDate: "12 May, 2021",
+        endDate: "24 May, 2021",
+        description: "Mount Everest is Earth's highest mountain above sea level, located in the Mahalangur Himal sub-range of the Himalayas. The China–Nepal border runs across its summit point. Its elevation of 8,848.86 m was most recently established in 2020 by the Chinese and Nepali authorities.",
+        imageUrl: "../images/Mount_Everest.jpg"
     }
 ]
