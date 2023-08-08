68dbe9487fac702f4033d5f80409ca64165edac5
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-08-07 21:02:36 -0600

    Google links update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,14 @@
 // import React from "react"
 
 export default function Card(props) {
+    let date = `${props.item.startDate} - ${props.item.endDate}`; // Fixed the date formatting
     return (
         <div className="main--container">
-            <img src={`../images/${props.item.imageUrl}`} className="card--image" />
+            <img src={`../images/${props.item.imageUrl}`} className="card--image" alt="Card Image" />
             <div className="card--stats">
-                <img src="" className="card--star" />
+                <img src="" className="card--star" alt="Star Icon" />
                 <span className="card--location">{props.item.location}</span>
-                <a className="card--url" href={props.item.googleMapsUrl}>{props.item.googleMapsUrl}</a>
+                <a className="card--url" href={date}>View on Google Maps</a>
                 <h1 className="card--title">{props.item.title}</h1> 
                 <span className="card--date">{props.item.startDate}</span>
                 <span className="card--date">{props.item.endDate}</span>
