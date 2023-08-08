d6c2f19be1926169606b7a810e061a2c0e25d5b5
Evgenii Kozhushko <climbersrock.eugene@gmail.com>
2023-08-08 16:48:28 -0600

    Final commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,15 @@
 // import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
 export default function Card(props) {
     let date = `${props.item.startDate} -  ${props.item.endDate}`
+    const isLastCard = props.isLast
     return (
         <div className={`main--container ${props.isLast ? 'last-main-container' : ''}`}>
             <img src={`../images/${props.item.imageUrl}`} className="card--image" />
             <div className="card--stats">
-                <img src="" className="card--star" />
+                <FontAwesomeIcon icon={faLocationDot} className="location--icon"/>
                 <span className="card--location">{props.item.location}</span>
                 <a className="card--url" href={props.item.googleMapsUrl}>View on Google Maps</a>
                 <h1 className="card--title">{props.item.title}</h1> 
@@ -16,3 +19,4 @@ export default function Card(props) {
         </div>
     )
 }
+
